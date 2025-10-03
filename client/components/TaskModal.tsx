ddba262312d6b050e6f5b989e7c138bc028cc3b9
Prasoon Tripathi <72148974+ProgrammerGuy3009@users.noreply.github.com>
2025-10-03 15:25:02 +0530

    Update TaskModal.tsx

--- client/components/TaskModal.tsx
+++ client/components/TaskModal.tsx
@@ -35,7 +35,7 @@ export default function TaskModal({ isOpen, onClose, onSubmit, task }: TaskModal
     if (task) {
       setFormData({
         ...task,
-        dueDate: task.dueDate ? task.dueDate.split('T')[0] : '' // Format date for input
+        dueDate: task.dueDate ? task.dueDate.split('T')[0] : '', // Format date for input
         completed: typeof task.completed === 'boolean' ? task.completed : false
       });
     } else {
