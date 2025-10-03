18fb7dbe272ef24a59629e8ecc7fbff882a726ac
Prasoon Tripathi <72148974+ProgrammerGuy3009@users.noreply.github.com>
2025-10-03 19:57:42 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -51,8 +51,8 @@ const userSchema = new mongoose.Schema({
 });
 
 // Index for better performance
-userSchema.index({ email: 1 });
-userSchema.index({ username: 1 });
+// userSchema.index({ email: 1 });
+// userSchema.index({ username: 1 });
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
